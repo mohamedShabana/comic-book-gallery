fd2ecc951ca44f4bde4edf75081d77e4b24810b4
Mohamed Monir shabana <mohamed_ms2006@hotmail.com>
2018-06-20 18:35:53 +0200

    add View to Action Method to start view

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
-            {
-                return Redirect("/");
-                //return new RedirectResult("/");
-            }
-            return Content("Hello from the comic books controller");
-           
+            return View();
         }
     }
 }
